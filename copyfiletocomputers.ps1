ab9bce3e7fa1a57dfb9c1492344ae0cba7b17b9e
Davis <Nathan.Davis@ingramcontent.com>
2017-01-19 11:49:20 -0600

    Added file copy script

---
+++ copyfiletocomputers.ps1
@@ -0,0 +1,12 @@
+﻿# Point the script to the text file
+$Computers = Read-Host "Enter Location Of TXT File"
+
+# sets the varible for the file location ei c:\temp\ThisFile.exe
+$Source = Read-Host "Enter File Source"
+
+# sets the varible for the file destination
+$Destination = Read-Host "Enter File destination (windows\temp)"
+
+
+# displays the computer names on screen
+Get-Content $Computers | foreach {Copy-Item $Source -Destination \\$_\c$\$Destination}
