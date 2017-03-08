77425d9fdc1d77909a3224c7a6322295761f946b
Nathan Davis <nathanwdavis@gmail.com>
2017-03-08 12:52:35 -0600

    created folder for remove hidden folders

---
+++ remove_hidden_adapters/remove_hidden_adapters.ps1
@@ -0,0 +1,21 @@
+﻿Set-Location C:\_Source\DeviceManagement
+
+Import-Module .\DeviceManagement.psd1 -Verbose
+
+# List Hidden Devices
+
+Get-Device -ControlOptions DIGCF_ALLCLASSES | Sort-Object -Property Name | Where-Object {($_.IsPresent -eq $false) -and ($_.Name -like “Microsoft Hyper-V Network Adapter*”) } | ft Name, DriverVersion, DriverProvider, IsPresent, HasProblem, InstanceId -AutoSize
+
+# Get Hidden Hyper-V Net Devices
+
+$hiddenHypVNics = Get-Device -ControlOptions DIGCF_ALLCLASSES | Sort-Object -Property Name | Where-Object {($_.IsPresent -eq $false) -and ($_.Name -like “Microsoft Hyper-V Network Adapter*”) }
+
+# Loop and remove with DevCon.exe
+
+ForEach ($hiddenNic In $hiddenHypVNics) {
+
+$deviceid = “@” + $hiddenNic.InstanceId
+
+.\devcon.exe -r remove $deviceid
+
+}
