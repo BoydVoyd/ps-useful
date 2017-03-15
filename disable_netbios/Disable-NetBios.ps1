837eebe29c90f777b5ca365cfa9dc9117870ff76
Nathan Davis <nathanwdavis@gmail.com>
2017-03-15 15:06:21 -0500

    finalized reporting in disable netbios script

--- disable_netbios/Disable-NetBios.ps1
+++ disable_netbios/Disable-NetBios.ps1
@@ -11,7 +11,27 @@
     $OpenFileDialog.filename
 }
 
-#$mydocs = [Environment]::GetFolderPath("MyDocuments")
-$inputfile = Get-FileName  "C:\Users\ndavis\Source\Repos\psuseful" "Choose a text file with a list computers"
 
-$inputfile
+$startfolder = [Environment]::GetFolderPath("MyDocuments")
+$inputfile = Get-FileName  $startfolder "Choose a text file with a list computers"
+
+Set-Location $startfolder
+
+Invoke-Command -ComputerName (Get-Content $inputfile) -ErrorAction SilentlyContinue -ErrorVariable ProcessError -scriptblock {
+#    Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces\tcpip* -Name NetbiosOptions
+    $results = set-ItemProperty HKLM:\SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces\tcpip* -Name NetbiosOptions -Value 2 -PassThru
+    $infoObjects = @()
+
+
+    foreach($result in $results){
+        $infoObject = New-Object PSObject
+        Add-Member -inputObject $infoObject -memberType NoteProperty -name "NetworkInterface" -value $result.PSChildName
+        Add-Member -inputObject $infoObject -memberType NoteProperty -name "NetbiosOptions" -value $result.NetbiosOptions
+        $infoObjects += $infoObject 
+    }
+
+    $infoObjects
+} | Select-Object -Property PSComputerName, NetworkInterface, NetbiosOptions | Export-Csv ".\netbios script results.csv" -NoTypeInformation
+if($error){
+    $ProcessError | Select-Object -Property TargetObject, FullyQualifiedErrorId, ErrorDetails | Export-CSV ".\netbios script exceptions.csv" -NoTypeInformation
+}
