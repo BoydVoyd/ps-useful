26e6e6600b0bee04aeeddc252249a73ecb9e06d5
Nathan Davis <nathanwdavis@gmail.com>
2017-05-18 13:25:12 -0500

    updated netbios script

--- disable_netbios/Disable-NetBios.ps1
+++ disable_netbios/Disable-NetBios.ps1
@@ -1,23 +1,48 @@
-﻿Function Get-FileName($initialDirectory,$Title)
-{
-    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
-    
-    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-    $OpenFileDialog.initialDirectory = $initialDirectory
-    $OpenFileDialog.title = $Title
-    $OpenFileDialog.filter = "CSV (*.csv)| *.csv|TXT (*.txt)|*.txt"
-    $OpenFileDialog.filterindex = 2
-    $OpenFileDialog.ShowDialog() | Out-Null
-    $OpenFileDialog.filename
-}
+﻿<#	
+	.NOTES
+	===========================================================================
+	Created on:		04-06-2017
+	Last Edited:	05-16-2017
+	Version:		1.4
+	Created by:		Nathan Davis
+	Organization:	Ingram Content Group
+	Filename:		Disable-Netbios.ps1
+	Changes: 		
+	===========================================================================
+	.SYNOPSIS
+		Disable Netbios on a list of Windows computers.
+	
+	.DESCRIPTION
+		Disable Netbios on a list of Windows computers.
+
+	.PARAMETER 
+        serverFile - A text file containing the names of the servers to be added. Default is servers.txt in the 
+        folder where the script is located.
+        outputDir - Directory to put the script logs. Default is the folder where the script is located.
+
+	.INPUTS
+		Text file with the name of the servers to have netbios diabled.
+
+	.OUTPUTS
+		netbios script results.csv - Contains results of succesful changes.
+        netbios script exceptions.csv - Contains errors when attemtped changes are unsuccesful.
+		
+	.EXAMPLE
+	.\Disable-Netbios.ps1
+    .\Disable-Netbios -serverFile "C:\scripts\data\servers.txt"
+#>
+
+Param(
+
+    [string]$outputDir=(Split-Path $MyInvocation.MyCommand.Path),
+    [string]$serverFile= (Split-Path $MyInvocation.MyCommand.Path) + '\servers.txt'
+)
+
+$resutlsFile = $outputDir + "\netbios script results.csv"
+$errorFile = $outputDir + "\netbios script exceptions.csv"
 
 
-$startfolder = [Environment]::GetFolderPath("MyDocuments")
-$inputfile = Get-FileName  $startfolder "Choose a text file with a list computers"
-
-Set-Location $startfolder
-
-Invoke-Command -ComputerName (Get-Content $inputfile) -ErrorAction SilentlyContinue -ErrorVariable ProcessError -scriptblock {
+Invoke-Command -ComputerName (Get-Content $serverFile) -ErrorAction SilentlyContinue -ErrorVariable ProcessError -scriptblock {
 #    Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces\tcpip* -Name NetbiosOptions
     $results = set-ItemProperty HKLM:\SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces\tcpip* -Name NetbiosOptions -Value 2 -PassThru
     $infoObjects = @()
@@ -31,7 +56,7 @@ Invoke-Command -ComputerName (Get-Content $inputfile) -ErrorAction SilentlyConti
     }
 
     $infoObjects
-} | Select-Object -Property PSComputerName, NetworkInterface, NetbiosOptions | Export-Csv ".\netbios script results.csv" -NoTypeInformation
+} | Select-Object -Property PSComputerName, NetworkInterface, NetbiosOptions | Export-Csv $resutlsFile -NoTypeInformation
 if($error){
-    $ProcessError | Select-Object -Property TargetObject, FullyQualifiedErrorId, ErrorDetails | Export-CSV ".\netbios script exceptions.csv" -NoTypeInformation
+    $ProcessError | Select-Object -Property TargetObject, FullyQualifiedErrorId, ErrorDetails | Export-CSV $errorFile -NoTypeInformation
 }
---
+++ test.ps1
@@ -0,0 +1,9 @@
+﻿Param(
+
+    [string]$outputDir=(Split-Path $MyInvocation.MyCommand.Path),
+    [string]$serverFile= $scriptDir + '\servers.txt'
+)
+$outputDir
+$serverFile
+$servers = Get-Content $serverFile
+$servers
